fbd28a7d23b4f18e263d4803483e5a8e3b62d43f
ahballif <addisonballif@gmail.com>
2025-10-25 09:49:44 -0600

    added basic grid

--- objects/tile.js
+++ objects/tile.js
@@ -1,21 +1,45 @@
-class Tile {
+export default class Tile extends Phaser.GameObjects.Container {
 
-    constructor(xi, yi) {
-        this.card = null
-        this.coordx = xi
-        this.coordy = yi
+    constructor(scene, gridOriginX, gridOriginY, xi, yi, gridwidth, gridheight) {
 
-        this.title_type = 0
+        super(scene, gridOriginX + xi*gridwidth, gridOriginY + yi*gridheight);
+
+
+        this.screenx = gridOriginX + xi*gridwidth;
+        this.screeny = gridOriginY + yi*gridheight;
+        
+        this.gridwidth = gridwidth;
+        this.gridheight = gridheight;
+
+        
+        this.coordx = xi;
+        this.coordy = yi;
+
+        this.card = null; // initial card at location
+
+        this.title_type = 0;
         // 0 means its not a playable tile
         // 1 means its a regular tile
         // 2 means its a tile that ends the game
+
+        this.drawTile();
     }
 
     drawTile() {
-        if (this.card != null) {
-            // Draw the card image
-            this.card
-        }
+        
+        this.tileImage = this.scene.add.image(this.screenx, this.screeny, "Back Tile.png");
+        this.tileImage.setDisplaySize(this.gridwidth, this.gridheight);
+
+        this.scene.add.existing(this);
+
+
+
+    }
+
+    updateCardOnTile() {
+        // moves card to right spot
+        this.card.x = this.screenx;
+        this.card.y = this.screeny;
     }
 
     
--- scenes/MainScene.js
+++ scenes/MainScene.js
@@ -1,91 +1,98 @@
 import Card from "../objects/card.js";
+import Tile from "../objects/tile.js"
 
 export default class MainScene extends Phaser.Scene {
-constructor() {
-  super("MainScene");
-}
-
-preload() {
-  // Load arrows
-  this.load.image("arrow_up", "assets/arrow_up.png");
-  this.load.image("arrow_down", "assets/arrow_down.png");
-  this.load.image("arrow_left", "assets/arrow_left.png");
-  this.load.image("arrow_right", "assets/arrow_right.png");
-
-  // Optional: if you have double-arrow versions
-  this.load.image("arrow_up_double", "assets/arrow_up_double.png");
-  this.load.image("arrow_down_double", "assets/arrow_down_double.png");
-  this.load.image("arrow_left_double", "assets/arrow_left_double.png");
-  this.load.image("arrow_right_double", "assets/arrow_right_double.png");
-
-  // Load card images from folder
-  CARD_LIBRARY.forEach(card => {
-    this.load.image(card.name, `Cards/${card.image}`);
-  });
-}
-
-create() {
-
-
-
-  let gridHeight = 5
-  let gridWidth = 5
-
-  
-  // set up the board 
-
-  const grid = [];
-  for (let r = 0; r < gridHeight; r++) {
-    const row = [];
-    for (let c = 0; c < gridWidth; c++) {
-      row.push(0); // or whatever initial value you want
-    }
-    grid.push(row);
+  constructor() {
+    super("MainScene");
   }
 
-  // set up the players
+  preload() {
+    // Load arrows
+    this.load.image("arrow_up", "assets/arrow_up.png");
+    this.load.image("arrow_down", "assets/arrow_down.png");
+    this.load.image("arrow_left", "assets/arrow_left.png");
+    this.load.image("arrow_right", "assets/arrow_right.png");
 
-  // For testing: pick random cards
-const p1hand = Phaser.Utils.Array.Shuffle(CARD_LIBRARY).slice(0, 5);
-const p2hand = Phaser.Utils.Array.Shuffle(CARD_LIBRARY).slice(5, 10);
+    // Optional: if you have double-arrow versions
+    this.load.image("arrow_up_double", "assets/arrow_up_double.png");
+    this.load.image("arrow_down_double", "assets/arrow_down_double.png");
+    this.load.image("arrow_left_double", "assets/arrow_left_double.png");
+    this.load.image("arrow_right_double", "assets/arrow_right_double.png");
 
-// Create both hands
-this.createHand(p1hand, 400, 700, false); // bottom hand
-this.createHand(p2hand, 400, 100, false);  // top hand
-
-
-  this.turnIsP1 = true;
-  
-
-
-  // Pick a card to visualize
-  const example = CARD_LIBRARY[5]; // or random one
-  this.add.text(20, 20, "Card Visual Test", { color: "#fff" });
-
-  
-
-
-  // Create and show card in center
-  const card = new Card(this, 400, 400, example);
-}
-
-
-createHand(cards, centerX, y, flipped) {
-const spacing = 120; // horizontal distance between cards
-const totalWidth = (cards.length - 1) * spacing;
-const startX = centerX - totalWidth / 2;
-
-cards.forEach((cardData, i) => {
-  const x = startX + i * spacing;
-  const card = new Card(this, x, y, cardData);
-
-  if (flipped) {
-    // Flip vertically (for top player)
-    card.setScale(-1, -1);
+    // Load card images from folder
+    CARD_LIBRARY.forEach(card => {
+      this.load.image(card.name, `Cards/${card.image}`);
+    });
   }
-});
 
-}
+  create() {
+
+    // set up the board 
+
+    
+
+    // set up the players
+
+    // For testing: pick random cards
+    const p1hand = Phaser.Utils.Array.Shuffle(CARD_LIBRARY).slice(0, 5);
+    const p2hand = Phaser.Utils.Array.Shuffle(CARD_LIBRARY).slice(5, 10);
+
+    // Create both hands
+    this.createHand(p1hand, 400, 700, false); // bottom hand
+    this.createHand(p2hand, 400, 100, false);  // top hand
+
+
+    this.turnIsP1 = true;
+
+
+
+    // Pick a card to visualize
+    const example = CARD_LIBRARY[5]; // or random one
+    this.add.text(20, 20, "Card Visual Test", { color: "#fff" });
+
+
+
+
+    // Create and show card in center
+    const card = new Card(this, 400, 400, example);
+
+
+    this.createGrid(100, 100, 50, 50, 5, 5);
+  }
+
+
+  createHand(cards, centerX, y, flipped) {
+    const spacing = 120; // horizontal distance between cards
+    const totalWidth = (cards.length - 1) * spacing;
+    const startX = centerX - totalWidth / 2;
+
+    cards.forEach((cardData, i) => {
+      const x = startX + i * spacing;
+      const card = new Card(this, x, y, cardData);
+
+      if (flipped) {
+        // Flip vertically (for top player)
+        card.setScale(-1, -1);
+      }
+    });
+
+
+  }
+
+  // draw the grid
+  createGrid(gridOriginX, gridOriginY, tilewidth, tileheight, nx, ny) {
+
+    const grid = [];
+      for (let r = 0; r < ny; r++) {
+        const row = [];
+        for (let c = 0; c < nx; c++) {
+          row.push(new Tile(this, gridOriginX, gridOriginY, c, r, tilewidth, tileheight)); // or whatever initial value you want
+        }
+        grid.push(row);
+      }
+
+
+  }
 
 
 
@@ -119,7 +126,7 @@ cards.forEach((cardData, i) => {
       }
 
       // now if its not a bomb, then push the next card over
-      if (card.cardtype[pushdirection-1] != "3") {
+      if (card.cardtype[pushdirection - 1] != "3") {
         this.movecard(nextcard, next_newx, next_newy, pushdirection);
       }
 
