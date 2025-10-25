64b0f8947b69e54ca1858d4121773493c2feb8f7
bustedchickn <talmage.kay@gmail.com>
2025-10-25 16:18:16 -0600

    Update maps.js

--- data/maps.js
+++ data/maps.js
@@ -28,5 +28,35 @@ export const TILE_MAP = [
         { type: "dirt", playable: 0 }
       ]
     ]
+  },
+  {
+    title: "Picnic 2",
+    playable_image: "picnic_tile",
+    map: [
+      [
+        { type: "dirt", playable: 0 },
+        { type: "grass", playable: 1 },
+        { type: "grass", playable: 1 },
+        { type: "grass", playable: 1 }
+      ],
+      [
+        { type: "grass", playable: 1 },
+        { type: "picnic", playable: 2 },
+        { type: "picnic", playable: 2 },
+        { type: "grass", playable: 1 }
+      ],
+      [
+        { type: "grass", playable: 1 },
+        { type: "grass", playable: 1 },
+        { type: "picnic", playable: 2 },
+        { type: "grass", playable: 1 }
+      ],
+      [
+        { type: "dirt", playable: 0 },
+        { type: "grass", playable: 1 },
+        { type: "grass", playable: 1 },
+        { type: "dirt", playable: 0 }
+      ]
+    ]
   }
 ];
